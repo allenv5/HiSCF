b95e037e3240e0480892a452926071163bac2d75
Allen Hu <allenv5@users.noreply.github.com>
2020-10-03 10:49:38 +0800

    Add files via upload

--- python/MclInputGenerator.py
+++ python/MclInputGenerator.py
@@ -1,19 +1,15 @@
-import os
-
 if __name__ == '__main__':
-    root_dir = "data\\triangle"
-    dataset = 'arabidopsis'
-    tran_matrix_file_template = os.path.join(root_dir, '{0}-tran_matrix')
-    dic_file_template = os.path.join(root_dir, '{0}-dic')
-    mcl_abc_file_template = '{0}-mcl-abc'
+    tran_matrix_file = ''
+    dic_file = ''
+    outputfile = ''
 
-    f = open(dic_file_template.format(dataset))
-    idDic = {}
+    f = open(dic_file)
+    id_dic = {}
     for line in f:
         items = line.split(" ")
-        idDic[int(items[1])] = items[0]
+        id_dic[int(items[1])] = items[0]
 
-    f = open(tran_matrix_file_template.format(dataset))
+    f = open(tran_matrix_file)
     array = [line.split("\t") for line in f]
 
     content = ''
@@ -25,10 +21,9 @@ if __name__ == '__main__':
         for j in range(0, len(array[i])):
             cell = float(array[i][j])
             if cell > 0:
-                content += ("" if len(content) == 0 else "\n") + idDic[i + 1] + "\t" \
-                           + idDic[j + 1] + "\t" + str(cell / sum)
+                content += ("" if len(content) == 0 else "\n") + id_dic[i + 1] + "\t" \
+                           + id_dic[j + 1] + "\t" + str(cell / sum)
 
-    mcl_abc_file_path = os.path.join(root_dir, mcl_abc_file_template).format(dataset)
-    mcl_abc_file = open(mcl_abc_file_path, 'w')
+    mcl_abc_file = open(outputfile, 'w')
     mcl_abc_file.write(content)
     mcl_abc_file.close()
